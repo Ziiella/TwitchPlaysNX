1c6c6d60a2ad8bfd4f2b4bd9e040ebf8ca20869e
Ziella Rose <ziellatheneko@gmail.com>
2019-04-27 22:02:19 +1000

    Add project files.

---
+++ TwitchPlaysNX/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace TwitchPlaysNX
+{
+    class Program
+    {
+        static IPAddress server_address;
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter the IP address of your switch.");
+                Console.WriteLine("Example: >TwitchPlaysNX <your switch IP>");
+                return;
+            }
+            else
+            {
+                server_address = IPAddress.Parse(args[0]); 
+
+            }
+        }
+    }
+}
