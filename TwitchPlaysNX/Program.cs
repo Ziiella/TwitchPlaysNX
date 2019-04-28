c3ee08140cd62e49c32b0d8f2d316ba482b48846
Ziella Rose <ziellatheneko@gmail.com>
2019-04-28 18:38:10 +1000

    Arguments and wait commands.

--- TwitchPlaysNX/Program.cs
+++ TwitchPlaysNX/Program.cs
@@ -48,6 +48,7 @@ namespace TwitchPlaysNX
             { "RR", 1 << 22 },
             { "RD", 1 << 23 }
         };
+        static List<string[]> Commands = new List<string[]>();
 
 
 
@@ -55,6 +56,7 @@ namespace TwitchPlaysNX
         public static IPEndPoint ipe;
         public static Socket sock;
 
+        static long keyInputs = 0;
         public static long keyout;
         public static int dx_l = 385;
         public static int dy_l = 643;
@@ -66,7 +68,7 @@ namespace TwitchPlaysNX
 
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 0)
             {
                 Console.WriteLine("Please enter the IP address of your switch.");
                 Console.WriteLine("Example: >TwitchPlaysNX <your switch IP>");
@@ -74,10 +76,13 @@ namespace TwitchPlaysNX
             }
             else
             {
-                Connect(args[0], port);
+                //Connect(args[0], port);
+                Connect("10.0.0.103", port);
 
 
                 Thread sendThread = new Thread(new ThreadStart(SendInput));
+                Thread commandThread = new Thread(new ThreadStart(ManageCommands));
+                commandThread.Start();
                 sendThread.Start();
 
                 while (true)
@@ -92,17 +97,11 @@ namespace TwitchPlaysNX
                             return;
 
                         default:
-                            int value = keys[command];
-                            if (keys.ContainsKey(command))
-                            {
-                                keyout |= keys[command];
-                            }
+                            ParseCommand(command);
                             break;
                     }
-                    
 
-                    Thread.Sleep(100);
-                    keyout = 0;
+
                 }
 
 
@@ -138,7 +137,8 @@ namespace TwitchPlaysNX
 
         static void SendInput()
         {
-            while (true) {
+            while (true)
+            {
 
                 byte[] msg = new byte[26];
                 byte[] msgTemp = { 0x75, 0x32 };
@@ -155,6 +155,77 @@ namespace TwitchPlaysNX
             }
         }
 
+        static void ParseCommand(string command)
+        {
+            string[] commands = command.Split(';');
+            
+            for (int i = 0; i < commands.Count(); i++)
+            {
+                string[] args = commands[i].Split(' ');
 
+                if(args[0] != "wait")
+                {
+                    if (!keys.ContainsKey(args[0]))
+                    {
+                        return;
+                    }
+                }
+
+                Commands.Add(args);  
+            }
+            string[] doneArgs = { "DONE" };
+            Commands.Add(doneArgs);
+
+        }
+
+        static void ManageCommands()
+        {
+            while (true)
+            { 
+                while (Commands.Count > 0)
+                {
+                    Thread.Sleep(20);
+                    string[] args = Commands[0];
+                    if (args[0] == "wait")
+                    {
+                        Commands.RemoveAt(0);
+                        keyout = keyInputs; 
+                        Thread.Sleep(100);
+
+                        if (args.Count() == 2)
+                        {
+                            int num = int.Parse(args[1]);
+                            keyInputs = 0;
+                            keyout = 0;
+
+                            if (num > 10) num = 10;
+
+                            Thread.Sleep(num);
+                        }
+                        else
+                            keyInputs = 0;
+                            keyout = 0;
+                            Thread.Sleep(1000);
+                    }
+
+                    if (args[0] == "DONE")
+                    {
+                        keyout = keyInputs;
+                        Commands.RemoveAt(0);
+                        Thread.Sleep(100);
+                        keyInputs = 0;
+                        keyout = 0;
+                    }
+
+                    else if (keys.ContainsKey(args[0]))
+                    {
+                        keyInputs |= keys[args[0]];
+                        Commands.RemoveAt(0);
+                    }
+
+
+                }
+            }
+        }
     }
 }
